d3ad61afffe74c833deb35dbfea096e2c1d25e38
Robert Beckett <robert.beckett@onion.red>
2019-11-05 17:32:55 +0000

    Moved requirements above document.ready and added function to convert date-seconds to interval of time since post.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,46 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweet =  {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+// Will need to add a ticket to apply this to each post element
+// ever x period of time.
+const timeSince = function(seconds) {
+  if (seconds < 60) {
+    return 'Under a minute ago';
+  }
+  if (seconds < 3600) {
+    return Math.floor(seconds / 60) + ' minutes ago';
+  }
+  if (seconds < 86400) {
+    return Math.floor(seconds / 3600) + ' hours ago';
+  }
+  // All approximations from here, for now.
+  if (seconds < 2592000) {
+    return Math.floor(seconds / 86400) + ' days ago';
+  }
+  if (seconds < 31104000) {
+    return Math.floor(seconds / 2592000) + ' months ago';
+  }
+  return Math.floor(seconds / 31104000) + ' years ago';
+};
+
 $(document).ready(function() {
+  const dateNow = Date.now();
+  const dateCreated = new Date(tweet.created_at).getTime();
+  console.log((tweet.created_at));
+  const timeSincePost = dateNow - dateCreated;
+  // Going to implement changing from minutes -> hours -> days.
   const createTweetElement = function(tweet) {
     const tweetElement = $('<article></article>')
       .addClass('tweet');
@@ -18,7 +57,7 @@ $(document).ready(function() {
       </header>
       <p>${tweet.content.text}</p>
       <footer>
-        <time>${tweet.created_at}</time>
+        <time>${timeSince(timeSincePost)}</time>
       <menu>
         <figure>⚑</figure>
         <figure>⟲</figure>
@@ -30,18 +69,5 @@ $(document).ready(function() {
     $('.tweet-list').append(tweetElement);
   };
   
-  const tweet =  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-  
   createTweetElement(tweet);
 });
-
