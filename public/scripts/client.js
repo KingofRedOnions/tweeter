e7ccfee51b1a2745bfcfa0c9a41e965568e9ef85
Robert Beckett <robert.beckett@onion.red>
2019-11-05 21:13:36 +0000

    Invoked empty function and removed test tweets.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,42 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Ramsey",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@tb"
-    },
-    "content": {
-      "text": "Time can work, maybe?",
-    },
-    "created_at": 1572975901455
-  }
-];
 
 const submitTweet = function() {
   const $tweetForm = $('.new-tweet > form');
@@ -130,4 +94,5 @@ const createTweetElement = function(tweet) {
 $(document).ready(function() {
   renderTweets(data);
   submitTweet();
+  loadTweets();
 });
