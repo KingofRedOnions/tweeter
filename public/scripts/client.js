ef08c3f9fe94ba4cd25aaddf508db55cccd53913
Robert Beckett <robert.beckett@onion.red>
2019-11-05 04:31:36 +0000

    Implemented and tested hard coded createTweetElement.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = function(tweet) {
+$(document).ready(function() {
+  const createTweetElement = function(tweet) {
+    const tweetElement = $('<article></article>')
+      .addClass('tweet');
+  
+    tweetElement.append(
+      `
+      <header>
+        <img src=${tweet.user.avatars}>
+        <p>${tweet.user.name}</p>
+        <p>${tweet.user.handle}</p>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <time>${tweet.created_at}</time>
+      <menu>
+        <figure>⚑</figure>
+        <figure>⟲</figure>
+        <figure>❤</figure>
+      </menu>
+      `
+    );
+  
+    $('.tweet-list').append(tweetElement);
+  };
+  
+  const tweet =  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+  
+  createTweetElement(tweet);
+});
 
-};
