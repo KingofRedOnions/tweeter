03e8e5b67284bef9fe4ac7e5a4833663bec98f63
Robert Beckett <robert.beckett@onion.red>
2019-11-05 00:19:41 +0000

    Implemented remaining key ticker.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    
+  const tweetText = $(".new-tweet > form > textarea");
+  const tweetTicker = $(".counter");
+
+  tweetText.on('keydown', (key) => {
+    remainingChar = 140 - tweetText.val().length;
+    tweetTicker.text(remainingChar);
+  });
 });
