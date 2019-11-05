86c577a21bbb86b328f7e4ba5463873e205c8560
Robert Beckett <robert.beckett@onion.red>
2019-11-05 17:37:24 +0000

    Replaced test tweet wth a tweets object, moved createTweetElement code into its out function above document.ready()

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweet =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 // Will need to add a ticket to apply this to each post element
 // ever x period of time.
@@ -43,36 +57,36 @@ const renderTweets = function(tweets) {
 };
 
 const createTweetElement = function(tweet) {
+  const tweetElement = $('<article></article>')
+    .addClass('tweet');
 
+  tweetElement.append(
+    `
+    <header>
+      <img src=${tweet.user.avatars}>
+      <p>${tweet.user.name}</p>
+      <p>${tweet.user.handle}</p>
+    </header>
+    <p>${tweet.content.text}</p>
+    <footer>
+      <time>${timeSince(timeSincePost)}</time>
+    <menu>
+      <figure>⚑</figure>
+      <figure>⟲</figure>
+      <figure>❤</figure>
+    </menu>
+    `
+  );
+
+  return tweetElement;
 };
 
 $(document).ready(function() {
   const dateNow = Date.now();
   const dateCreated = new Date(tweet.created_at).getTime();
-  console.log((tweet.created_at));
   const timeSincePost = dateNow - dateCreated;
   // Going to implement changing from minutes -> hours -> days.
-  const createTweetElement = function(tweet) {
-    const tweetElement = $('<article></article>')
-      .addClass('tweet');
   
-    tweetElement.append(
-      `
-      <header>
-        <img src=${tweet.user.avatars}>
-        <p>${tweet.user.name}</p>
-        <p>${tweet.user.handle}</p>
-      </header>
-      <p>${tweet.content.text}</p>
-      <footer>
-        <time>${timeSince(timeSincePost)}</time>
-      <menu>
-        <figure>⚑</figure>
-        <figure>⟲</figure>
-        <figure>❤</figure>
-      </menu>
-      `
-    );
   
     $('.tweet-list').append(tweetElement);
   };
