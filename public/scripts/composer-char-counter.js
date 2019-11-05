ef201d511e92a673d2acd4bf574da005d0eea22d
Robert Beckett <robert.beckett@onion.red>
2019-11-05 00:28:20 +0000

    Changed listener from keyDown to keyUp to trade immediate results for more accurate results.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
   const tweetText = $(".new-tweet > form > textarea");
   const tweetTicker = $(".counter");
+  let remainingChar = 140;
 
-  tweetText.on('keydown', (key) => {
+  tweetText.on('keyup', (key) => {
     remainingChar = 140 - tweetText.val().length;
     tweetTicker.text(remainingChar);
+
+    if (remainingChar >= 0) {
+      tweetTicker.css('color', '#545149');
+    } else {
+      tweetTicker.css('color', 'red');
+    }
   });
 });
